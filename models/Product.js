e1352769fb7623f1469acf67a8f54566d2da1737
louiskgkim <lkgk31@gmail.com>
2023-02-08 17:42:43 -0800

    forgot category id

--- models/Product.js
+++ models/Product.js
@@ -35,6 +35,13 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
