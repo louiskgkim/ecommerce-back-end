92759a0a814a932970b8542b300ca5ca4924a865
louiskgkim <lkgk31@gmail.com>
2023-02-09 17:44:09 -0800

    was missing req.params.id in several routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk({ include: Product });
+    const tagData = await Tag.findByPk(req.params.id, { include: Product });
     if (!tagData) {
       res.status(404).json({ message: 'No tag by this ID!' });
       return;
