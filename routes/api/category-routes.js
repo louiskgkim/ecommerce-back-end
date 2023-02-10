9a66b783f38c73078983eae42a7bbff69af978f6
louiskgkim <lkgk31@gmail.com>
2023-02-09 17:42:36 -0800

    fixed more errors

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk({ include: Product });
+    const categoryData = await Category.findByPk(req.params.id, { include: Product });
     if (!categoryData) {
       res.status(404).json({ message: 'No category by this ID!' });
       return;
